522b068752a3490f25871ecf4174db36cf8d4d0e
项小盆友 <1259653875@qq.com>
2018-08-15 09:55:41 +0800

    创建梦景项目

---
+++ InDream/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  InDream
+//
+//  Created by 项小盆友 on 2018/8/15.
+//  Copyright © 2018年 med-vision. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '8.0'
+target 'InDream' do
+pod 'AFNetworking'
+pod 'YYModel'
+pod 'Masonry'
+pod 'MBProgressHUD'
+pod 'SDWebImage'
+pod 'IQKeyboardManager'
+pod 'MJRefresh'
+end 
+
